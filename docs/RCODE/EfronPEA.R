fc816fc2350d826fa58a10e2f30363877d0e9cbf
Aldo Solari <solari.aldo@gmail.com>
2022-03-07 13:52:46 +0100

    update

--- docs/RCODE/EfronPEA.R
+++ docs/RCODE/EfronPEA.R
@@ -36,20 +36,20 @@ dataset <- read_table2("https://hastie.su.domains/CASI_files/DATA/cholesterol.tx
   rename(x = compliance, y = cholesterol.decrease)
 fit <- lm(y ~ poly(x,3), dataset)
 x_grid <- data.frame(x=seq(min(dataset$x),max(dataset$x), length=100))
-Shat <- predict(fit, newdata = x_grid, se = TRUE)
+S_hat <- predict(fit, newdata = x_grid, se = TRUE)
 
 #pdf("Figure_1.pdf")
 plot(y~x, dataset, 
      pch = ".",
      xlab="normalized compliance", 
      ylab="cholesterol decrease")
-lines(x_grid$x,Shat$fit, lwd=2)
+lines(x_grid$x,S_hat$fit, lwd=2)
 legend("topleft",
        legend=paste("Adj Rsquared = ",round(summary(fit)$adj.r.squared,3)))
 ix = seq(10,90,length=11)
 for (i in 1:11){
 segments(x0 = x_grid$x[ix[i]], x1 = x_grid$x[ix[i]],
-         y0 = Shat$fit[ix[i]] - Shat$se.fit[ix[i]], y1 = Shat$fit[ix[i]] + Shat$se.fit[ix[i]],
+         y0 = S_hat$fit[ix[i]] - S_hat$se.fit[ix[i]], y1 = S_hat$fit[ix[i]] + S_hat$se.fit[ix[i]],
          col=2, lwd=2)
 }
 #dev.off()
